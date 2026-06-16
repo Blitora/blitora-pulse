1419ec2e4f47f0a7ee407c2f83a8207d2ca4430f
BiteBhavan <azeemsayyed1990@gmail.com>
2026-06-16 22:26:37 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -66,7 +66,11 @@ export default function Login() {
       const { error } = await sb.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: window.location.origin + "/auth/callback"
+          redirectTo: `${window.location.origin}/auth/callback`,
+          queryParams: {
+            access_type: "offline",
+            prompt: "consent",
+          },
         }
       });
       if (error) { setError(error.message); setLoading(false); }
